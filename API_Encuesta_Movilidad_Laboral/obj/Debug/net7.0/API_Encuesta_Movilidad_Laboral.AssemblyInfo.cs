5580505054e148ad9a43bcb2fb0302c67b1c3f8c
Pablo Barra Urtubia <pbarrau.90@icloud.com>
2025-09-14 21:35:30 -0300

    mod: modificacion de la cadena de cx en la base de datos

--- API_Encuesta_Movilidad_Laboral/obj/Debug/net7.0/API_Encuesta_Movilidad_Laboral.AssemblyInfo.cs
+++ API_Encuesta_Movilidad_Laboral/obj/Debug/net7.0/API_Encuesta_Movilidad_Laboral.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_Encuesta_Movilidad_Laboral")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74930cf297a65b243fdb46c4dc4910fa3dd18154")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_Encuesta_Movilidad_Laboral")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_Encuesta_Movilidad_Laboral")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
